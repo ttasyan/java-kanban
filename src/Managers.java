6f93351ad452f4fd70051cd8cf62893667cb32fe
Avdulova <ttasina00@mail.ru>
2024-07-28 18:15:38 +0300

    ТЗ 6 спринта

--- src/Managers.java
+++ src/Managers.java
@@ -2,6 +2,7 @@ public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
