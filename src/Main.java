15f5ffb121c613404ce47c9b112bb990bfec98a3
Avdulova <ttasina00@mail.ru>
2024-08-23 01:29:24 +0300

    ТЗ 8 спринта

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-import managers.InMemoryTaskManager;
-import managers.TaskManager;
-import tasks.Epic;
-import tasks.SubTask;
-import tasks.Task;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
